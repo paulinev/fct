736ae032b1ac44bf50dc21abbcc3fe955d23def5
Ubuntu <ubuntu@ip-10-118-33-175.ec2.internal>
2013-10-04 16:01:11 +0000

    cleaning up structure

---
+++ Links/create_static_link.py
@@ -0,0 +1,7 @@
+
+def create_static_link(max_time, step, filename):
+    with open(filename, 'w') as f:
+        for i in range(0, max_time+1, step):
+            f.write(str(i)+'\n')
+
+create_static_link(140000, 1, "static_link_1.ms")
