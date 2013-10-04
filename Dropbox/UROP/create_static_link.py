8f321947bbd05048fb49bf429c0526f2de200342
Ubuntu <ubuntu@ip-10-118-33-175.ec2.internal>
2013-10-04 15:55:24 +0000

    adding more flowreqs

--- Dropbox/UROP/create_static_link.py
+++ Dropbox/UROP/create_static_link.py
@@ -4,4 +4,4 @@ def create_static_link(max_time, step, filename):
         for i in range(0, max_time+1, step):
             f.write(str(i)+'\n')
 
-create_static_link(140000, 10, "static_link_10.ms")
+create_static_link(140000, 1, "static_link_1.ms")
